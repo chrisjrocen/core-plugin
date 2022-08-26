15463fb7cd6ec19b4e8a1cc70cba16cb91192327
chrisocen <105217816+chrisocen@users.noreply.github.com>
2022-08-27 01:21:45 +0300

    created projects post type

---
+++ core-plugin.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Plugin Name: Core Plugin
+ * Plugin URI:  https://ocenchris.com
+ * Description: Core Plugin
+ * Author:      Ocen Chris
+ * Author URI:  https://ocenchris.com
+ * Version:     1.0.0
+ * License:     1.0.0
+ * text-domain: core-plugin
+ *
+ * @package coreplugin.
+ */
+
+// If this file is called directly, abort!!!
+defined( 'ABSPATH' ) || die( 'No Access!' );
+
+if ( 'PLUGIN_URL' ) {
+	define( 'PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+}
+
+//includes
+require_once( plugin_dir_path( __FILE__ ) . '/core/post-types/projects.php' );
+
+//Hooks and Filters
+add_action( 'init', 'register_projects_post_type' );
+
+
+//Shortcodes
+
+
+
---
+++ core/post-types/projects.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Add Custom Post Type.
+ *
+ * @package coreplugin.
+ */
+
+/*****************************************************************************
+Register an extra post type for content with taxonomy
+ *****************************************************************************/
+
+function register_projects_post_type() {
+	// Project 
+	$labels = array(
+		'name'               => __( 'Projects', 'core-plugin' ),
+		'singular_name'      => __( 'Project', 'core-plugin' ),
+		'add_new'            => __( 'New Project', 'core-plugin' ),
+		'add_new_item'       => __( 'Add New Project', 'core-plugin' ),
+		'edit_item'          => __( 'Edit Project', 'core-plugin' ),
+		'new_item'           => __( 'New Project', 'core-plugin' ),
+		'view_item'          => __( 'View Project', 'core-plugin' ),
+		'search_items'       => __( 'Search Projects', 'core-plugin' ),
+		'not_found'          => __( 'No Project Found', 'core-plugin' ),
+		'not_found_in_trash' => __( 'No Project found in Trash', 'core-plugin' ),
+	);
+	$args = array(
+		'label'               => __( 'Projects', 'core-plugin' ),
+		'labels'              => $labels,
+		'description'         => __( 'Projects you have worked on', 'core-plugin'),
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_admin_bar'   => true,
+		'has_archive'         => false,
+		'public'              => true,
+		'publicly_queryable'  => true,
+		'can_export'          => true,
+		'show_in_rest'        => true,
+		'exclude_from_search' => false,
+		'hierarchical'        => false,
+		'menu_icon'           => 'dashicons-analytics',
+		'capability_type'     => 'post',
+		'taxonomies'          => array( 'category' ),
+		'rewrite'             => array( 'slug' => 'projects' ),
+		'supports'            => array( 
+			'title',
+			'custom-fields',
+			'thumbnail',
+		),
+
+	);
+	register_post_type( 'projects', $args );
+}
